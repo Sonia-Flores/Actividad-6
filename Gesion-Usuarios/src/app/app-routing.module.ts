70c3c09e6fe188a1ab2b1af1d371836379e1ba3c
Sonia-GE <sflores@grupoepelsa.com>
2023-09-03 18:36:49 +0200

    Rutas y componentes

--- Gesion-Usuarios/src/app/app-routing.module.ts
+++ Gesion-Usuarios/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { FormComponent } from './pages/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'newuser', component: FormComponent },
+  { path: 'uptadeuser/:id', component: FormComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Gesion-Usuarios/src/app/app.module.ts
+++ Gesion-Usuarios/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
+import { FormComponent } from './pages/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    HomeComponent,
+    UserComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
